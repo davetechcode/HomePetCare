ccc0fd8cb8ec07befd304422b2e4cf57c662ad55
martincode <86748030+martinjoyavi@users.noreply.github.com>
2021-10-11 17:24:21 -0500

    Update Persona.cs
    Elimina espacios vacíos

--- HomePetCare.App/HomePetCare.App.Dominio/Entidades/Persona.cs
+++ HomePetCare.App/HomePetCare.App.Dominio/Entidades/Persona.cs
@@ -4,14 +4,12 @@ namespace HomePetCare.App.Dominio
 {
     public class Persona
     {
-        public  int Id { get; set; }
+        public int Id { get; set; }
         public string identificacion { get; set;}
         public string Nombre { get; set; }
         public string Apellidos { get; set; }
         public string Direccion { get; set; }
-        public string Telefono { get; set; } 
-        // 
-        
+        public string Telefono { get; set; }
 
     }
 
