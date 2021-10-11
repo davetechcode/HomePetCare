89eeceec0392b8a2492a1c998969bf1846eef511
martincode <86748030+martinjoyavi@users.noreply.github.com>
2021-10-11 17:31:13 -0500

    Frontend (#2)
    * Base para poder trabajar con backend
    
    * Formularios completos, estilos en proceso.
    
    * Base con todos los elementos del crud menos eliminar
    
    * Update Persona.cs
    
    Elimina espacios vacíos
    
    * Update Persona.cs
    
    Un código más pegadito 😁💜
    
    * Update Persona.cs
    
    Formato de código más pegado.
    
    Co-authored-by: Juan Varon <juanvarong@gmail.com>

--- HomePetCare.App/HomePetCare.App.Dominio/Entidades/Persona.cs
+++ HomePetCare.App/HomePetCare.App.Dominio/Entidades/Persona.cs
@@ -1,18 +1,13 @@
 using System;
-
 namespace HomePetCare.App.Dominio
 {
     public class Persona
     {
-        public  int Id { get; set; }
-        public string identificacion { get; set;}
-        public string Nombre { get; set; }
-        public string Apellidos { get; set; }
-        public string Direccion { get; set; }
-        public string Telefono { get; set; } 
-        // 
-        
-
+        public int Id {get;set;}
+        public string identificacion {get;set;}
+        public string Nombre {get;set;}
+        public string Apellidos {get;set;}
+        public string Direccion {get;set;}
+        public string Telefono {get;set;}
     }
-
 }
