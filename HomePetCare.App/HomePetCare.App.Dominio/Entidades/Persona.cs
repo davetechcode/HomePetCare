aaa44895e7ce11e8aa41b41b125f4c9df2b618b2
brayan <boliveros203@gmail.com>
2021-10-11 22:47:04 -0500

    ajuste

--- HomePetCare.App/HomePetCare.App.Dominio/Entidades/Persona.cs
+++ HomePetCare.App/HomePetCare.App.Dominio/Entidades/Persona.cs
@@ -4,14 +4,11 @@ namespace HomePetCare.App.Dominio
 {
     public class Persona
     {
-        public  int Id { get; set; }
-        public string Identificacion { get; set;}
-        public string Nombre { get; set; }
-        public string Apellidos { get; set; }
-        public string Direccion { get; set; }
-        public string Telefono { get; set; }
-        
-
+        public int Id {get;set;}
+        public string Identificacion {get;set;}
+        public string Nombre {get;set;}
+        public string Apellidos {get;set;}
+        public string Direccion {get;set;}
+        public string Telefono {get;set;}
     }
-
 }
