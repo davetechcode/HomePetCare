f14236445d21f19dc0a8419cd0bf50b7663383f9
baoc203 <90145310+baoc203@users.noreply.github.com>
2021-10-11 22:39:05 -0500

    Update Persona.cs

--- HomePetCare.App/HomePetCare.App.Dominio/Entidades/Persona.cs
+++ HomePetCare.App/HomePetCare.App.Dominio/Entidades/Persona.cs
@@ -3,7 +3,7 @@ namespace HomePetCare.App.Dominio
 {
     public class Persona
     {
-        public  int Id {get;set;}
+        public int Id {get;set;}
         public string Identificacion {get;set;}
         public string Nombre {get;set;}
         public string Apellidos {get;set;}
